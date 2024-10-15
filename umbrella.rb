1fff6382e0fc221c9ec7a7b7071ee712abfa1371
waikyjung <waiky.jung@gmail.com>
2024-10-15 00:00:10 +0000

    response code

--- umbrella.rb
+++ umbrella.rb
@@ -5,8 +5,12 @@ require "date"
 require "ascii_charts"
 
 class Weather
-  attr_accessor :google_http, :weather_http
+  attr_accessor :google_http, :weather_http, :current_time_edit, :current_temp, :temps
   
+  def initialize
+    self.start
+  end
+
   def get_coords(place)
     @google_http = "https://maps.googleapis.com/maps/api/geocode/json?address="
     @gmaps_key = ENV.fetch("GMAPS_KEY")
@@ -14,6 +18,10 @@ class Weather
 
     @google_request = @google_http + @place.gsub(" ", "%20") + "&key=" + @gmaps_key
     @google_raw_response = HTTP.get(@google_request)
+    if @google_raw_response.code == 200
+      puts "#{@place} not found. Please restart and try again."
+      exit
+    end
     @google_response = JSON.parse(@google_raw_response)
     @geometry = @google_response.fetch("results")[0].fetch("geometry")
     
@@ -112,4 +120,3 @@ class Weather
 end
 
 check_weather = Weather.new
-check_weather.start
